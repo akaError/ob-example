30ca91f1bb2c68123632cae7906062d79918500e
linzhigang.lzg <linzhigang.lzg@alibaba-inc.com>
2022-08-22 17:24:02 +0800

    add go env

---
+++ go/go_env.sh
@@ -0,0 +1,2 @@
+go env -w GO111MODULE=auto
+echo "export GOPATH=/workspace/ob-example/go" >> ~/.bash_profile && source ~/.bash_profile
---
+++ go/test.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+    "database/sql"
+    "fmt"
+    "log"
+    
+    _ "oceanbase/mysql" //填写 go-sql-driver/mysql 安装的准确路径。如果安装在 src 目录下，可以直接填 "mysql"。
+)
+
+type City struct {
+    Id         int
+    Name       string
+    Population int
+}
+
+func main() {
+    select_all()
+    }
+
+func select_all() {
+    conn := "root:@tcp(127.0.0.1:34905)/test"
+    db, err := sql.Open("mysql", conn)
+    if err != nil {
+        log.Fatal(err)
+    }
+    
+    defer db.Close()
+    
+    if err != nil {
+        log.Fatal(err)
+    }
+    
+    res, err := db.Query("SELECT * FROM cities")
+    if err != nil {
+        log.Fatal(err)
+    }
+    
+    defer res.Close()
+    
+    if err != nil {
+        log.Fatal(err)
+    }
+    
+    for res.Next() {
+        
+        var city City
+        err := res.Scan(&city.Id, &city.Name, &city.Population)
+        
+        if err != nil {
+            log.Fatal(err)
+        }
+        
+        fmt.Printf("%v\n", city)
+    }
+}
