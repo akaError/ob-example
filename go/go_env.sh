7ea830d3016bfa5325c33f608a5b0656cf9d0dc4
linzhigang.lzg <linzhigang.lzg@alibaba-inc.com>
2022-08-23 15:29:28 +0800

    update go env

--- go/go_env.sh
+++ go/go_env.sh
@@ -1,3 +1,5 @@
 go env -w GO111MODULE=auto
-echo "export GOPATH=/workspace/ob-example/go" >> ~/.bash_profile
- source ~/.bash_profile
+
+echo "export GOPATH=/workspace/ob-example/go" >> ~/.profile
+
+source ~/.profile
