1324fc105153dd7342bf1b98b3efb27d5cabc3fc
linzhigang.lzg <linzhigang.lzg@alibaba-inc.com>
2022-08-25 15:42:22 +0800

    update

--- python/Test.py
+++ python/Test.py
@@ -4,7 +4,7 @@ import pymysql
 
 conn = pymysql.connect(host="localhost", port=2881,
                        user="root", passwd="", db="test")
-
+print("success to connect OceanBase with pymysql")
 try:
     with conn.cursor() as cur:
         cur.execute('drop table if exists test')
