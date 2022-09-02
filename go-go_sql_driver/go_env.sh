3c6d4679d44e8f7936e52e30b51894c7b5df725d
linzhigang.lzg <linzhigang.lzg@alibaba-inc.com>
2022-09-02 17:37:26 +0800

    update

--- go-go_sql_driver/go_env.sh
+++ go-go_sql_driver/go_env.sh
@@ -1,5 +1,6 @@
 gopath=$(cd `dirname $0`;pwd)
 go env -w GO111MODULE=auto
 echo "export GOPATH="$gopath >> ~/.profile
+echo "alias sh='/bin/bash'" >> ~/.profile
 source ~/.profile
 go get github.com/go-sql-driver/mysql
--- python3-pymysql/py_env.sh
+++ python3-pymysql/py_env.sh
@@ -1,1 +1,3 @@
+echo "alias sh='/bin/bash'" >> ~/.profile
+source ~/.profile
 pip3 install pymysql
