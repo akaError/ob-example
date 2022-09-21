e918af9f1354b8adf38fda46a771ec8925b0051d
linzhigang.lzg <linzhigang.lzg@alibaba-inc.com>
2022-09-21 16:58:22 +0800

    te

--- go-go_sql_driver/env.sh
+++ go-go_sql_driver/env.sh
@@ -1,6 +1,8 @@
 gopath=$(cd `dirname $0`;pwd)
 mkdir src
 echo "export GOPATH="$gopath"" >> ~/.bash_profile
-source ~/.profile
-export GOPROXY=https://goproxy.io&&export GO111MODULE=on
+source ~/.bash_profile
+go env -w GO111MODULE=auto
+export GOPROXY=https://goproxy.io
 go get github.com/go-sql-driver/mysql
+#go install github.com/go-sql-driver/mysql@latest
