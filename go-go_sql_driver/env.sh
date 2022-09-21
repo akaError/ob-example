489828e50984ba3211484b199d19ad07e3519bcf
akaError <97499944+akaError@users.noreply.github.com>
2022-09-21 17:20:16 +0800

    Update env.sh

--- go-go_sql_driver/env.sh
+++ go-go_sql_driver/env.sh
@@ -3,6 +3,7 @@ mkdir src
 echo "export GOPATH="$gopath"" >> ~/.bash_profile
 source ~/.bash_profile
 go env -w GO111MODULE=auto
+export GO111MODULE=auto
 export GOPROXY=https://goproxy.io
 go get github.com/go-sql-driver/mysql
 #go install github.com/go-sql-driver/mysql@latest
